747d79741a4eacbae2c482ebeeae981aa38392bf
MaiAnhTu96 <maianhtu96@gmail.com>
2016-11-27 14:30:56 +0700

    Firt  Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    interface ibook
+    {
+        string this[int index]
+        {
+            get; set;
+        }
+        string Title
+        {
+            get; set;
+        }
+        string Author
+        {
+            get; set;
+        }
+        string Publisher
+        {
+            get; set;
+        }
+        string ISBN
+        {
+            get; set;
+        }
+        int Year
+        {
+            get; set;
+        }
+        void show();
+    } //INTERFACE (PROPERTY,METHOD)
+
+    class book : ibook //LOP BOOK KE THUA IBOOK
+    {
+        #region        //REGION DINH NGHIA DU LIEU KHAC CHO BOOK ~ IBOOK
+
+        private string isbn;
+        private string title;
+        private string author;
+        private string publisher;
+        private int year;
+        private ArrayList chapter = new ArrayList();
+
+        #endregion
+
+        #region        //REGION DINH NGHIA LAI DU LIEU PROPERTY CUA INTERFACE (IBOOK)
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < chapter.Count)
+                    return (string)chapter[index];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < chapter.Count)
+                    chapter[index] = value;
+                else if (index == chapter.Count)
+                    chapter.Add(value);
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+        public string Title
+        {
+            get; set;
+        }
+        public string Author
+        {
+            get; set;
+        }
+        public string Publisher
+        {
+            get; set;
+        }
+        public string ISBN
+        {
+            get; set;
+        }
+        public int Year
+        {
+            get; set;
+        }
+
+        public void show()  //SHOW INFOMATION ABOUT BOOK
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Title    :" + title);
+            Console.WriteLine("Author   :" + author);
+            Console.WriteLine("Publisher:" + publisher);
+            Console.WriteLine("Year     :" + year);
+            Console.WriteLine("ISBN     :" + isbn);
+            Console.WriteLine("Chapter:  : ");
+            for (int i = 0; i < chapter.Count; i++)
+                Console.WriteLine("\tCHUONG {0} {1}", i + 1, chapter[i]);
+            Console.WriteLine("---------------------------");
+        }
+        public void input() //XUAT DANH SACH BOOK
+        {
+            Console.Write("Title    : ");
+            title = Console.ReadLine();
+            Console.Write("Author   : ");
+            author = Console.ReadLine();
+            Console.Write("Publisher: ");
+            publisher = Console.ReadLine();
+            Console.Write("ISBN     : ");
+            isbn = Console.ReadLine();
+            Console.Write("Year     : ");
+            year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Da Nhap Sach Thanh Cong\nNhan 2 lan de ket thuc");
+            string str;
+            do
+            {
+                str = Console.ReadLine();
+                if (str.Length > 0)
+                    chapter.Add(str);
+            } while (str.Length > 0);
+        }
+        class booklist      //CLASS THAO TAC DS SACH
+        {
+            private ArrayList list = new ArrayList(); //MANG ARRAY
+            public void addbook()
+            {
+                book b = new book();
+                b.input();
+                list.Add(b);
+            } //Them Book
+            public void inputlist()
+            {
+                int n;
+                Console.Write("So luong of books: ");
+                n = int.Parse(Console.ReadLine());
+                while (n > 0)
+                {
+                    addbook();
+                    n--;
+                }
+            } //Them 1 luong n sach
+            public void showlist()
+            {
+                foreach (book b in list)
+                    b.show();
+            } //Xuat DS n sach
+            
+        } //CLASS BOOKLIST QUAN LY DANH SACH DOI TUONG BOOK
+        class program
+        {
+            static void Main(string[] args)     //HAM CHINH
+            {
+                booklist b1 = new booklist();
+                b1.inputlist();
+
+                b1.showlist();
+
+                Console.ReadLine();
+            }
+        }    
+    }
+}
+#endregion
---
+++ TINHTOAN/TINHTOAN/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TINHTOAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string t;
+            t = Convert.ToString(textBox1.Text);
+            if (t == "")
+            {
+                MessageBox.Show("CHUA NHAP GI !!! ", "MESSENGER", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.textBox1.Focus();
+            }
+            else
+            {
+                int sum;
+                sum = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                textBox3.Text = sum.ToString();
+                btnC.Enabled = false;
+            }          
+         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult h= MessageBox.Show("BAN MUON THOAT HAY KHONG", "THOAT", MessageBoxButtons.YesNo);
+            if (h==DialogResult.Yes)
+                this.Close();
+            else
+                this.textBox1.Focus();
+        }
+        //THAO TAC TINH TOAN
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int tru;
+            tru = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+            textBox3.Text = tru.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nhan;
+            nhan = int.Parse(textBox1.Text) * int.Parse(textBox2.Text);
+            textBox3.Text = nhan.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            float chia;
+            chia = float.Parse(textBox1.Text) / float.Parse(textBox2.Text);
+            textBox3.Text = chia.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            btnC.Enabled = true;
+        }
+
+       
+    }
+}
